3b0ba87a098321aa045ca0b9270a2ff713b4ac18
Radhika garg <158601174+Radhikagarg933@users.noreply.github.com>
2026-08-14 22:48:32 +0530

    Create rat.java

---
+++ rat.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int unit=sc.nextInt();
+        int r=sc.nextInt();
+        int[]a=new int[n];
+        
+        int i;
+        
+        for( i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        if(n==0)System.out.println(-1);
+        int sum=0;
+        int t=r*unit;
+        int c=0;
+        for(i=0;i<n;i++)
+        { c++;
+            sum=sum+a[i];
+            if(sum>t)
+            {
+                break;
+            }
+        }
+System.out.println(c);
+}
+}
